a9e58bea8f0858b9ecd7f6daf3f329ab82f5aaa4
Pavlos Georgiou <pavlos.georgiou@couchbase.com>
2026-04-09 18:00:48 +0100

    Remove vagrant cluster
    It is out of date

--- vagrant/gorgon_couchbase/control/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-apt-get -qy update
-apt-get -qy install curl golang-go netcat-openbsd python3 sudo tar unzip wget
-
-cd src/gorgon_couchbase
-go build
--- vagrant/gorgon_couchbase/node/rpc-server.sh
+++
@@ -1,9 +0,0 @@
-set -o errexit
-set -o pipefail
-set -o nounset
-
-cd /home/vagrant
-
-until nc -q 1 localhost 8091 < /dev/null ; do sleep 1 ; done
-
-src/gorgon_couchbase/gorgon_couchbase rpc
