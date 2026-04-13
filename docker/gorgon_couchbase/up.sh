8d858a78cb5b3c8b28c7cac1246e769acd3e43fe
Chirag Agarwal <chirag.agarwal@couchbase.com>
2026-04-13 13:57:27 +0530

    Add Jenkinsfile

--- docker/gorgon_couchbase/up.sh
+++ docker/gorgon_couchbase/up.sh
@@ -9,4 +9,7 @@ pwd
 
 make DB_NODES=${DB_NODES:-3}
 
-docker compose -f compose.yaml up --force-recreate $*
+docker compose -f compose.yaml up --force-recreate --abort-on-container-exit $*
+
+exit_code=$(docker compose -f compose.yaml ps --exit-code control 2>/dev/null || echo "1")
+exit $exit_code
